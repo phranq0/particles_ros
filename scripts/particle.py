f8482195bb6ce8d1e9bd697cedcfcb34fd0711bd
phranq0 <francesco95grella@gmail.com>
2021-02-12 16:02:31 +0100

    Tested offline basic filter

---
+++ scripts/particle.py
@@ -0,0 +1,95 @@
+# All the components for the particle filter are found here 
+import numpy as np
+from numpy.random import uniform
+from numpy.random import randn
+import scipy
+import random
+
+# Initial uniform distribution of particles
+def create_uniform_particles(x_range,y_range,theta_range,N):
+    particles = np.empty((N,3))
+    particles[:,0] = uniform(x_range[0], x_range[1], size=N)
+    particles[:,1] = uniform(y_range[0], y_range[1], size=N)
+    particles[:,2] = uniform(theta_range[0], theta_range[1], size=N)
+    particles[:,2] %= 2*np.pi       # Normalize wrt pi for heading
+    return particles
+# TEST
+#print(create_uniform_particles((0,1),(0,1),(0,2*np.pi),4))
+
+# Initial gaussian distribution of particles
+def create_gaussian_particles(mean, std, N):
+    particles = np.empty((N,3))
+    particles[:,0] = mean[0] + (randn(N)*std[0])
+    particles[:,1] = mean[1] + (randn(N)*std[1])
+    particles[:,2] = mean[2] + (randn(N)*std[2])
+    particles[:,2] %= 2*np.pi
+    return particles
+# TEST
+#print(create_gaussian_particles((0.0,0.0,0.0),(0.3,0.3,0.1),10))
+
+# Predict Step
+# Steps the motion model, which is the state transition of each particle
+# with noise
+# Input is in the shape (w,v)
+def predict(particles, u, std, dt=1.):
+    N = len(particles)
+    particles[:,2] += u[0] + (randn(N)*std[0])
+    particles[:,2] %= 2*np.pi                  # Always normalize angular variables
+    dist = (u[1]*dt) + (randn(N)*std[1])
+    particles[:,0] += np.cos(particles[:,2])*dist
+    particles[:,1] += np.sin(particles[:,2])*dist
+
+# Update step (for now related to landmark-based model, 
+# to be decoupled for suiting more sensor models)
+def update(particles,weights,z,R,landmarks):
+    # For each landmark
+    for i, landmark in enumerate(landmarks):
+        distance = np.linalg.norm(particles[:,0:2] - landmark, axis=1)  # P(z|x)
+        weights *= scipy.stats.norm(distance,R).pdf(z[i])               # P(z|x)*P(x)
+
+    weights += 1.e-300  
+    weights /= sum(weights)     # [P(z|x)*P(x)]/P(z)
+
+# State estimation
+# Exploits the weight update to compute an estimate of the real state
+# In most cases a weighted sum is enough
+def estimate(particles, weights):
+    # Just tracking (x,y) position
+    pos = particles[:,0:2]
+    mean = np.average(pos, weights=weights, axis=0)
+    var = np.average((pos-mean)**2, weights=weights, axis=0)
+    return mean, var
+# TEST
+# particles = create_uniform_particles((0,1),(0,1),(0,5),1000)
+# weights = np.array([1.0/1000.0]*1000)
+# [mean,var] = estimate(particles,weights)
+
+# Resampling step
+# Used to avoid degenerancy of the particle set
+def multinomial_resample(particles, weights):
+    N = len(particles)
+    # Cumulative sum to have a sorted array
+    cumulative_sum = np.cumsum(weights)
+    cumulative_sum[-1] = 1.0  # For avoiding round-off
+    indexes = np.searchsorted(cumulative_sum, randn(N))
+    # Resampling
+    particles[:] = particles[indexes]
+    weights.fill(1.0/N)
+
+# Takes indexes which are output of a resampling algorithm
+def resample_from_index(particles,weights,indexes):
+    # Only selected particles survive
+    particles[:] = particles[indexes]
+    # Weights are normalized again
+    weights.resize(len(particles))
+    weights.fill (1.0 / len(weights))
+
+# Resampling trigger
+# Many metrics can be adopted, the basic one is 
+# computing the number of effective (i.e. useful) particles
+def neff(weights):
+    return 1.0/np.sum(np.square(weights))
+
+
+
+
---
+++ scripts/particle_test.py
@@ -0,0 +1,86 @@
+# Script for testing the basic particle filtering algorithm
+# This does not require ros 
+from particle import *
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy.random import seed
+from numpy.linalg import norm
+from numpy.random import randn
+import scipy.stats
+
+# Resampling algorithms from filterpy
+from filterpy.monte_carlo import systematic_resample
+
+# Basic filtering test loop
+def run_sim_pf(N, iters=30, sensor_std_err=0.1, do_plot=True, plot_particles=False,
+            xlim=(0,20), ylim=(0,20), initial_x=None):
+    landmarks = np.array([[-1, 2], [5, 10], [12,14], [18,21]])
+    NL = len(landmarks)      
+    plt.figure()
+
+    # --------------------------------Initialize filter elements
+    # Gaussian or Uniform
+    if initial_x is not None:
+        particles = create_gaussian_particles(
+            mean=initial_x, std=(5, 5, np.pi/4), N=N)
+    else:
+        particles = create_uniform_particles((0,20), (0,20), (0, 6.28), N)
+    # Initial normalized weights
+    weights = np.ones(N) / N
+
+    # Plot initial particles distribution
+    # if plot_particles:
+    #     alpha = .20
+    #     if N > 5000:
+    #         alpha *= np.sqrt(5000)/np.sqrt(N)           
+    #     plt.scatter(particles[:, 0], particles[:, 1], 
+    #                 alpha=alpha, color='g')
+
+    xs = []                         # Particle set
+    robot_pos = np.array([0., 0.])  # Initial robot position
+
+    # Simulation and estimation loop
+    for x in range(iters):
+        # Evolve robot position
+        robot_pos += (1, 1)
+
+        # Measurements (distance from landmarks)
+        zs = (norm(landmarks - robot_pos, axis=1) + (randn(NL) * sensor_std_err))
+
+        # Evolve particles following the motion model
+        predict(particles, u=(0.00, 1.414), std=(.2, .05))
+        
+        # Update weights from measurements
+        update(particles, weights, z=zs, R=sensor_std_err, landmarks=landmarks)
+        
+        # Resample triggering
+        if neff(weights) < N/2:
+            indexes = systematic_resample(weights)
+            resample_from_index(particles, weights, indexes)
+            #multinomial_resample(particles, weights)
+            #assert np.allclose(weights, 1/N)
+
+        # State estimation
+        mu, var = estimate(particles, weights)
+        xs.append(mu)
+
+        if plot_particles:
+            plt.scatter(particles[:, 0], particles[:, 1], 
+                        color='k', marker=',', s=1)
+
+        # Plot ground truth and estimate
+        p1 = plt.scatter(robot_pos[0], robot_pos[1], marker='+',
+                         color='b', s=180, lw=3)
+        p2 = plt.scatter(mu[0], mu[1], marker='s', color='r')
+
+    # Plot data after loop
+    xs = np.array(xs)
+    plt.legend([p1,p2], ['Actual','PF'], loc=4, numpoints=1)
+    plt.xlim(*xlim)
+    plt.ylim(*ylim)
+    print('final position error, variance:\n\t', mu - np.array([iters, iters]), var)
+    plt.show()
+
+# Run simulation
+seed(6)
+run_sim_pf(N=5000,plot_particles=True,initial_x=(1,1, np.pi/4))
